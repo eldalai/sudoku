f896060cb1773a819a0a916e3faa46ab65ffff1f
gabriel <eldalai@gmail.com>
2013-01-30 12:12:34 -0300

    Sudoku board validation

---
+++ src/main/java/com/gl/exercice/sudoku/SudokuBoard.java
@@ -0,0 +1,102 @@
+package com.gl.exercice.sudoku;
+
+public class SudokuBoard {
+
+	public static final int MAX_LENGHT = 9;
+	private int[][] board ;
+
+	/**
+	 * Create a Sudoku Board
+	 * @param board: simple 9 x 9 character string
+	 */
+	public SudokuBoard( String initialBoard ) {
+		super();
+		board = new int[MAX_LENGHT][MAX_LENGHT];
+
+		int x1 = 0;
+		int number;
+		for( int fil = 0; fil <= (MAX_LENGHT - 1); fil++ )
+			for( int col = 0; col <= (MAX_LENGHT - 1); col++ ) {
+				number = Integer.parseInt( initialBoard.substring( x1, x1+1 ) );
+				board[fil][col] = number; 
+				x1++;
+			}
+
+	}
+
+	/**
+	 * valid Board
+	 * @return true is valid fils, cols & sectors  
+	 */
+	public boolean isValid() {
+		return isColsValid() && isFilsValid() && isSectorsValid();
+	}
+	
+	/**
+	 * valid cols Board
+	 * @return true is valid cols  
+	 */
+	public boolean isColsValid() {
+		int [] ctl = new int[10];
+		for( int col = 0; col <= (MAX_LENGHT - 1); col++ ){
+			cleanControl(ctl);
+			for( int fil = 0; fil <= (MAX_LENGHT - 1); fil++ ){
+				ctl[ board[fil][col] ]++;
+				if(ctl[ board[fil][col] ]!=1) {
+					System.out.println("Ctrl x col: Invalid in:"+fil+" col:"+col+" num:"+board[fil][col]);
+					return false;
+				}
+			}
+		}
+		return true;
+	}		
+	
+	/**
+	 * valid fils Board
+	 * @return true is valid fils  
+	 */
+	public boolean isFilsValid() {
+		int [] ctl = new int[10];
+		for( int fil = 0; fil <= (MAX_LENGHT - 1); fil++ ){
+			cleanControl(ctl);
+			for( int col = 0; col <= (MAX_LENGHT - 1); col++ ){
+				ctl[ board[fil][col] ]++;
+				if(ctl[ board[fil][col] ]!=1) {
+					System.out.println("Ctrl x fil: Invalid in fil:"+fil+" col:"+col+" num:"+board[fil][col]);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * valid sectors Board
+	 * @return true is valid sectors  
+	 */
+	public boolean isSectorsValid() {
+		int [] ctl = new int[10];
+		
+		for( int col = 0; col < 3; col++ ){
+			for( int fil = 0; fil < 3; fil++ ){
+				cleanControl(ctl);
+				for( int icol = 0; icol < 3; icol++ ) {
+					for( int ifil = 0; ifil < 3; ifil++ ) {
+						ctl[ board[fil*3+ifil][col*3+icol] ]++;
+						if(ctl[ board[fil*3+ifil][col*3+icol] ]!=1) {
+							System.out.println("Ctrl x sector: Invalid in fil:"+fil+" col:"+col+" num:"+board[fil*3+ifil][col*3+icol] );
+							return false;
+						}
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
+
+	private void cleanControl(int[] ctl) {
+		for( int clean = 0; clean <= MAX_LENGHT; clean++ ) ctl[clean]=0;
+	}
+	
+}
--- src/test/java/com/gl/exercice/sudoku/test/TestBoard.java
+++ src/test/java/com/gl/exercice/sudoku/test/TestBoard.java
@@ -1,5 +1,7 @@
 package com.gl.exercice.sudoku.test;
 
+import com.gl.exercice.sudoku.SudokuBoard;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -28,10 +30,143 @@ public class TestBoard extends TestCase {
 	    }
 
 	    /**
-	     * Rigourous Test :-)
+	     * Test a Valid Sudoku Board
 	     */
-	    public void testApp()
+	    public void testValidSudokuBoard()
 	    {
-	        assertTrue( true );
+	    	SudokuBoard board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertTrue( board.isValid() );
+	        assertTrue( board.isColsValid() );
+	        assertTrue( board.isFilsValid() );
+	        assertTrue( board.isSectorsValid() );
 	    }
-	}
+
+	    /**
+	     * Test a invalid col Sudoku Board
+	     */
+	    public void testInvalidColSudokuBoard()
+	    {
+	    	SudokuBoard board = new SudokuBoard(
+	    			"534678912" + // invalid col 1
+	    			"572195348" + // invalid col 1
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isColsValid() );
+
+	    	board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" + // invalid col 3
+	    			"198342567" +
+	    			"852761423" + // invalid col 3
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isColsValid() );
+
+	        board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" + // invalid col 9
+	    			"961537284" +
+	    			"287419636" + // invalid col 9
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isColsValid() );
+	    }
+
+	    /**
+	     * Test a invalid fil Sudoku Board
+	     */
+	    public void testInvalidFilSudokuBoard() {
+	    	SudokuBoard board = new SudokuBoard(
+	    			"554678912" + // invalid fil
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isFilsValid() );
+	    	
+	    	board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713927856" + // invalid fil
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isFilsValid() );
+	        
+	        board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286199"); // invalid fil
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isFilsValid() );
+	    }
+
+	    /**
+	     * Test a invalid sector Sudoku Board
+	     */
+	    public void testInvalidSectorSudokuBoard()
+	    {
+	    	SudokuBoard board = new SudokuBoard(
+	    			"534678912" + // 5 invalid
+	    			"652195348" + // 5 invalid
+	    			"198342567" +
+	    			"859761423" +
+	    			"426853791" +
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isSectorsValid() );
+	    	board = new SudokuBoard(
+	    			"534678912" +
+	    			"672195348" +
+	    			"198342567" +
+	    			"859761423" + // 7 is invalid in sector
+	    			"426857791" + // 7 is invalid in sector
+	    			"713924856" +
+	    			"961537284" +
+	    			"287419635" +
+	    			"345286179");
+	        assertFalse( board.isValid() );
+	        assertFalse( board.isSectorsValid() );
+	    }
+}
